e3d8215d58297577eb061b32d9aabc95324cbd6f
bradosia <bradosia@gmail.com>
2017-10-18 16:48:27 -0700

    List and Stack definitions moved

--- Stack.h
+++ Stack.h
@@ -16,129 +16,17 @@ private:
 	int itemCount = 0;
 public:
 
-	/** Sees whether the list is empty.
-	@return True if the list is empty, or false if not. */
-	bool isEmpty()
+	bool pop(T anEntry)
 	{
-		if (!tail)
-		{
-			return true;
-		}
-		return false;
+		return remove(anEntry);
 	}
-
-	/**Adds a new entry to the list.
-	post  If successful, newEntry is stored in the list and
-	the count of items in the list has increased by 1.
-	param newEntry The object to be added as a new entry.
-	return True if addition was successful, or false if not. */
-	bool add(T newEntry)
+	bool push(T newEntry)
 	{
-		Node<T> *newNode;
-		Node<T> *currentNode;
-		bool returnStatus = true;
-
-		newNode = new Node<T>(newEntry);
-
-		itemCount++;
-		if (isEmpty())
-		{
-			tail = newNode;
-			head = tail;
-		}
-		else
-		{
-			currentNode = tail;
-
-			while (currentNode->next)
-				currentNode = currentNode->next;
-
-			currentNode->next = newNode;
-			head = currentNode->next;
-		}
-		return returnStatus;
+		return add(newEntry);
 	}
-
-	/*Removes one occurrence of a given entry from this list, if possible.
-	post  If successful, anEntry has been removed from the list and the count of
-	items in the list has decreased by 1.
-	param anEntry  The entry to be removed.
-	return  True if removal was successful, or false if not.*/
-	bool remove(T anEntry)
+	bool isempty()
 	{
-		Node<T> *currentNode;
-		Node<T> *prevNode;
-
-		currentNode = tail;
-		prevNode = currentNode;
-		bool returnStatus = true;
-
-		if (currentNode->value == anEntry)
-		{
-			currentNode = currentNode->next;
-			delete tail;
-			tail = currentNode;
-		}
-		else
-		{
-
-			while (currentNode != nullptr && currentNode->value != anEntry)
-			{
-				prevNode = currentNode;
-				currentNode = currentNode->next;
-			}
-
-			if (currentNode->value == anEntry && head->value == anEntry)
-			{
-				prevNode->next = currentNode->next;
-				//delete currentNode;
-				//delete head;
-				head = prevNode;
-
-			}
-			else if (currentNode->value == anEntry)
-			{
-				prevNode->next = currentNode->next;
-				delete currentNode;
-			}
-		}
-		if (itemCount != 0)
-		{
-			itemCount--;
-		}
-		return returnStatus;
-	}
-
-	/*Counts the number of times a given entry appears in list.
-	param anEntry  The entry to be counted.
-	return  The number of times anEntry appears in the list. */
-	int getFrequencyOf() const
-	{
-		return itemCount;
-	}
-
-	/*Tests whether this list contains a given entry.
-	param anEntry  The entry to locate.
-	return  True if list contains anEntry, or false otherwise. */
-	bool contains(T anEntry)
-	{
-		Node<T> *currentNode;
-
-		currentNode = tail;
-
-
-		while (currentNode)
-		{
-			if (currentNode->value == anEntry)
-			{
-				return true;
-			}
-			else
-			{
-				currentNode = currentNode->next;
-			}
-		}
-		return false;
+		return isEmpty()
 	}
 	//To display the entire linked list
 	void displayList()
